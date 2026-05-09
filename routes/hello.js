1fbd5637feb4d2d721a6ab83edbf4b28d5f4054f
Sera <h.kaeru.s1321@gmail.com>
2026-05-09 16:27:26 +0900

    hello console

--- routes/hello.js
+++ routes/hello.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+console.log('HELLO ROUTE LOADED');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('hello', { title: 'Express' });
+  res.render('hello', { title: 'hello' });
 });
 
 module.exports = router;
