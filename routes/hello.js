8d529075691393dbb1a9242e3ce3e4c44f03442c
2401100020qn-oss <2401100020qn@cyber-u.ac.jp>
2026-05-09 16:24:04 +0900

    Update hello.js

--- routes/hello.js
+++ routes/hello.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+console.log('HELLO ROUTE LOADED');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('hello', { title: 'hello' });
